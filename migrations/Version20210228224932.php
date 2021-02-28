2d476afb5c3c76cf1d3461f3b0522e3bdb4bbd09
saidouux <oumakhlouf.said@gmail.com>
2021-03-01 00:28:01 +0100

    Add PHP config, Make Pin entity timestampable, Listing on index by createdAt desc

--- migrations/Version20210228224932.php
+++ migrations/Version20210228224932.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210226154414 extends AbstractMigration
+final class Version20210228224932 extends AbstractMigration
 {
     public function getDescription() : string
     {
