4fa098c32f31d98aeb760ff497ddd4cd14895d77
saidouux <oumakhlouf.said@gmail.com>
2021-03-10 12:36:18 +0100

    Add flah messages and move base layout to layouts folder

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -38,6 +38,8 @@ class PinsController extends AbstractController
             $em->persist($pin);
             $em->flush();
 
+            $this->addFlash('success', 'Pin successfully created !');
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -68,6 +70,9 @@ class PinsController extends AbstractController
         if($form->isSubmitted() && $form->isValid()) {            
             $em->flush();
 
+            $this->addFlash('success', 'Pin successfully updated !');
+
+
             return $this->redirectToRoute('app_home');
         }
 
@@ -86,6 +91,9 @@ class PinsController extends AbstractController
         if($this->isCsrfTokenValid('pin_deletion_' . $pin->getId(), $request->request->get('csrf_token'))) {
             $em->remove($pin);
             $em->flush();
+
+            $this->addFlash('info', 'Pin successfully deleted !');
+
         }
 
         return $this->redirectToRoute('app_home');
